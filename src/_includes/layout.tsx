51063066d5fc9722b816e1e92917bd8664241a81
ktortolini <134340991+ktortolini@users.noreply.github.com>
2024-04-07 15:26:17 -0500

    Add the layout tsx file in /_includes/

---
+++ src/_includes/layout.tsx
@@ -0,0 +1,22 @@
+export default ({ title, children }: Lume.Data, helpers: Lume.Helpers) => {
+	return (
+		<html data-theme = 'dark'>
+			<head>
+				<meta charSet = 'utf-8' />
+				<meta
+					name = 'viewport'
+					content = 'width=device-width, initial-scale=1'
+				/>
+				<title>{title}</title>
+				<link rel = 'stylesheet' href = '/unocss.css' />
+				<script src = 'https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.js' />
+				<script src = '/static/interactive-rose.js' defer />
+			</head>
+			<body className = 'flex bg-black justify-center'>
+				<div className= 'h-full text-center select-none all:transition-400'>
+					<main className = 'container'>{children}</main>
+				</div>
+			</body>
+		</html>
+	);
+};
